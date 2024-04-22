1ccc70e8cf6fe6d2647a465449996585f5bdf5ae
www-data <www-data@gittreesoft.com>
2024-04-21 22:20:22 -0700

    Version 8. First Version

--- two.c
+++ two.c
@@ -2,13 +2,9 @@
 
 int two()
 {
-	printf("two\n");
-<<<<<<< HEAD
-}
-=======
 		printf("two Edited on github.com\n");
 		printf("two try 22222\n");
 		printf("two try 3333\n");
 		printf("two\n");
 }
->>>>>>> bab2c6cfe9c38e888434d370305066b4193d14ef
+
